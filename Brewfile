420b42e6635d2db0e83984d5b2b3c2f372f1d199
Tiger Yotsawat <tigeryotsawat@gmail.com>
2021-09-03 21:59:58 +0700

    Initial commit

---
+++ Brewfile
@@ -0,0 +1,32 @@
+brew "python"
+brew "mysql"
+brew "node"
+brew "git"
+brew "mackup"
+brew "heroku/brew/heroku"
+
+cask "google-chrome"
+cask "google-drive"
+cask "google-backup-and-sync"
+cask "spotify"
+cask "spectacle"
+cask "iterm2"
+cask "postman"
+cask "zoom"
+cask "visual-studio-code"
+cask "mactex-no-gui"
+cask "discord"
+cask "trader-workstation"
+
+mas "CopyClip", id: 1020812363
+mas "GoodNotes", id: 1444383602
+mas "Keynote", id: 409183694
+mas "LINE", id: 539883307
+mas "Messenger", id: 1480068668
+mas "Numbers", id: 409203825
+mas "Pages", id: 409201541
+mas "Shazam", id: 897118787
+mas "Telegram", id: 747648890
+mas "Twitter", id: 1482454543
+mas "WeChat", id: 836500024
+mas "WhatsApp", id: 1147396723
---
+++ fresh.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+echo "Setting up your Mac..."
+
+if [ -f ~/.osx-bootstrapped.txt ]; then
+cat << EOF
+~/.osx-bootstrapped.txt FOUND!
+This laptop has already been bootstrapped.
+Exiting. No changes were made.
+EOF
+exit 0
+fi
+
+XCODE_IS_INSTALLED=$(which xcode-select)
+
+# Install Xcode
+if test ! $(which xcode-select); then
+  echo "Installing Xcode"
+  xcode-select --install
+fi
+
+# Check for Oh My Zsh and install if we don't have it
+if test ! $(which omz); then
+  echo "Installing Oh My Zsh"
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+fi
+
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  echo "Installing Homebrew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Update Homebrew recipes
+brew update
+
+# Install all our dependencies with bundle (See Brewfile)
+brew tap homebrew/bundle
+brew tap homebrew/cask
+brew install mas
+brew bundle
+brew upgrade
+brew cleanup
+
+npm install -g concurrently
+npm install -g nodemon
+npm install -g sass
+
+# Set default MySQL root password and auth type
+# mysql -u root -e "ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY 'password'; FLUSH PRIVILEGES;"
+
+# Install PHP extensions with PECL
+# pecl install imagick memcached redis swoole
+
+# Install global Composer packages
+# /usr/local/bin/composer global require laravel/installer laravel/valet beyondcode/expose
+
+# Install Laravel Valet
+# $HOME/.composer/vendor/bin/valet install
+
+# Create a Sites directory
+# mkdir $HOME/Sites
+
+# Create sites subdirectories
+# mkdir $HOME/Sites/blade-ui-kit
+# mkdir $HOME/Sites/eventsauce
+# mkdir $HOME/Sites/laravel
+
+# Clone Github repositories
+# ./clone.sh
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+# Symlink the Mackup config file to the home directory
+ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+
+source .vscode
+
+# Set macOS preferences - we will run this last because this will reload the shell
+source .macos
+
+touch $HOME/.osx-bootstrapped.txt
