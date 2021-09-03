c34139c19c862326b3a8618b05779fd547ad91ca
Tiger Yotsawat <tigeryotsawat@gmail.com>
2021-09-03 22:46:46 +0700

    Add vscode settings symlink

--- fresh.sh
+++ fresh.sh
@@ -73,11 +73,14 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.zshrc
+rm -f $HOME/.zshrc
 ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+
+rm -f $HOME/Library/Application\ Support/Code/User/settings.json
+ln -s $HOME/.dotfiles/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 
 source .vscode
 
