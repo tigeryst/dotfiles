b468545a0eb45d0a77c2ae1c6abfb03f5090a149
Tiger Yotsawat <tigeryotsawat@gmail.com>
2021-09-03 23:14:48 +0700

    Factor out config file for each new user:

--- fresh.sh
+++ fresh.sh
@@ -69,22 +69,8 @@ npm install -g sass
 # Clone Github repositories
 # ./clone.sh
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# source .vscode
 
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -f $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
-
-# Symlink the Mackup config file to the home directory
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
-
-rm -f $HOME/Library/Application\ Support/Code/User/settings.json
-ln -s $HOME/.dotfiles/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-
-source .vscode
-
-# Set macOS preferences - we will run this last because this will reload the shell
-source .macos
+./user-config.sh
 
 touch $HOME/.osx-bootstrapped.txt
