cdd05084df23207fdc7f3abf9762c0145a4b5987
Tiger Yotsawat <tigeryotsawat@gmail.com>
2023-10-13 23:30:23 +0700

    Fix relative path pointing to the wrong plugin directory

--- terminal/install.sh
+++ terminal/install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if [ -d plugins/zsh-syntax-highlighting ]; then
+if [ -d $HOME/.dotfiles/terminal/plugins/zsh-syntax-highlighting ]; then
     echo "zsh-syntax-highlighting is already installed"
 else
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.dotfiles/terminal/plugins/zsh-syntax-highlighting
 fi
 
-if [ -d plugins/zsh-autosuggestions ]; then
+if [ -d $HOME/.dotfiles/terminal/plugins/zsh-autosuggestions ]; then
     echo "zsh-autosuggestions is already installed"
 else
-    git clone https://github.com/zsh-users/zsh-autosuggestions plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.dotfiles/terminal/plugins/zsh-autosuggestions
 fi
