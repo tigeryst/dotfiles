d98414cb13a28561cbc62ae35adc5e909d65cac8
Tiger Yotsawat <tigeryotsawat@gmail.com>
2023-10-14 12:06:56 +0700

    Remove clone repository

--- git/clone.sh
+++ git/clone.sh
@@ -4,4 +4,4 @@ echo "Cloning repositories..."
 
 PROJECTS=$HOME/Projects
 
-git clone https://github.com/tigeryst/project-seshat.git $PROJECTS/project-seshat
+# git clone https://github.com/tigeryst/project-seshat.git $PROJECTS/project-seshat
