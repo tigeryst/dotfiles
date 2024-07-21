544a0bcf43a6a80058c8a34ded0ab1bb1079963d
Tiger Yotsawat <tigeryotsawat@gmail.com>
2024-07-22 01:32:53 +0700

    Fix aliases

--- terminal/aliases.zsh
+++ terminal/aliases.zsh
@@ -1,14 +1,13 @@
 # Shortcuts
-alias reloadshell="source $HOME/.zshrc"
+alias reshell="source $HOME/.zshrc"
 alias shrug="echo '¯\_(ツ)_/¯' | tee >(pbcopy)"
 alias relaunch="defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock"
-alias mkcd='mkdir -p "$1" && cd "$1"'
+mkcd() {
+    mkdir -p "$1" && cd "$1"
+}
 
 # Change directories
 alias dotfiles="cd $DOTFILES && code ."
-alias library="cd $HOME/Library"
-alias desktop="cd $HOME/Desktop"
-alias projects="cd $HOME/Projects"
 
 # JS
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
