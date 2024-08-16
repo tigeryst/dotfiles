f4bafb290517ce98d1c2bd3328d2be98963644f1
Tiger Yotsawat <tigeryotsawat@gmail.com>
2024-08-16 19:57:20 +0700

    Activate yarn and pnpm

--- javascript/install.sh
+++ javascript/install.sh
@@ -4,3 +4,7 @@ echo "Intalling global npm packages..."
 npm install -g npm-check-updates
 npm install -g n
 npm install -g typescript
+npm install -g corepack
+
+# Enable corepack for yarn and pnpm
+corepack enable
