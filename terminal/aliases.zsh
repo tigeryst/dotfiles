ca016d9d063929fe7204f2ca87469e5382f4517d
Tiger Yotsawat <tigeryotsawat@gmail.com>
2025-06-27 17:30:19 +0100

    Add ffmpeg and alias

--- terminal/aliases.zsh
+++ terminal/aliases.zsh
@@ -1,4 +1,6 @@
+# --------------------
 # Shortcuts
+# --------------------
 alias reshell="source $HOME/.zshrc"
 alias shrug="echo '¯\_(ツ)_/¯' | tee >(pbcopy)"
 alias relaunch="defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock"
@@ -6,12 +8,28 @@ mkcd() {
     mkdir -p "$1" && cd "$1"
 }
 
+# --------------------
 # Packages
+# --------------------
 alias aupdate="brew update && brew upgrade && brew cleanup && mas upgrade"
 
-# Change directories
+# --------------------
+# Directories
+# --------------------
 alias dotfiles="cd $DOTFILES && code ."
 
+# --------------------
 # JS
+# --------------------
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
 alias nupdate="ncu -u && npm update"
+
+# --------------------
+# Computer vision
+# --------------------
+play() {
+    local framerate="${1:-64}"            # default: 64 if no arg
+    local pattern="${2:-frame_%010d.jpg}" # default pattern if not provided
+    ffplay -framerate "$framerate" -i "$pattern"
+}
+
