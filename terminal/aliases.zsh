ac8f29fcfd0f7528141a66bae16345e116fdb2f8
Tiger Yotsawat <tigeryotsawat@gmail.com>
2025-07-14 17:06:41 +0100

    Remove tmux aliases

--- terminal/aliases.zsh
+++ terminal/aliases.zsh
@@ -33,32 +33,6 @@ play() {
     ffplay -framerate "$framerate" -i "$pattern"
 }
 
-# --------------------
-# Terminal multiplexer
-# --------------------
-
-# Start a new tmux session (with name)
-alias tnew='tmux new -s'
-
-# Attach to an existing tmux session (with name)
-alias tatt='tmux attach -t'
-
-# List all tmux sessions
-alias tls='tmux ls'
-
-# Kill a tmux session (with name)
-alias tkill='tmux kill-session -t'
-
-# Reattach if one exists, or create a new named session
-tstart() {
-    local name="$1"
-    if tmux has-session -t "$name" 2>/dev/null; then
-        tmux attach -t "$name"
-    else
-        tmux new -s "$name"
-    fi
-}
-
 # ---------------------
 # Remote
 # ---------------------
