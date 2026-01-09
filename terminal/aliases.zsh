a3afff3c6564920d8a0c531e804959c7ad3e3d79
Tiger Yotsawat <tigeryotsawat@gmail.com>
2026-01-09 23:02:47 +0700

    Remove unused aliases

--- terminal/aliases.zsh
+++ terminal/aliases.zsh
@@ -3,7 +3,6 @@
 # --------------------
 alias reshell="source $HOME/.zshrc"
 alias shrug="echo '¯\_(ツ)_/¯' | tee >(pbcopy)"
-alias relaunch="defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock"
 mkcd() {
     mkdir -p "$1" && cd "$1"
 }
@@ -40,55 +39,6 @@ trim_png() {
         return 1
     fi
     magick "$1" -trim +repage "$2"
-}
-
-# ---------------------
-# Remote
-# ---------------------
-
-sync_remote_dir() {
-    if [ "$#" -ne 4 ]; then
-        echo "Usage: syncdir push|pull user@remote /source/path/ /destination/path/"
-        return 1
-    fi
-
-    MODE="$1"
-    REMOTE="$2"
-    SOURCE="$3"
-    DEST="$4"
-
-    # Force SOURCE to end with trailing slash
-    [[ "${SOURCE}" != */ ]] && SOURCE="${SOURCE}/"
-
-    if [ "$MODE" = "push" ]; then
-        echo "Checking destination on remote..."
-        if ssh "$REMOTE" "[ -d \"$DEST\" ]"; then
-            echo "Destination exists. Using rsync to push changes..."
-            rsync -avz --delete "$SOURCE" "$REMOTE:\"$DEST\""
-        else
-            echo "Destination does not exist. Doing initial tar push..."
-            ssh "$REMOTE" "mkdir -p \"$DEST\""
-            # Archive the contents of the source folder, not the folder itself:
-            tar czf - -C "$SOURCE" . |
-                ssh "$REMOTE" "tar xzf - -C \"$DEST\""
-        fi
-
-    elif [ "$MODE" = "pull" ]; then
-        echo "Checking destination locally..."
-        if [ -d "$DEST" ]; then
-            echo "Destination exists. Using rsync to pull changes..."
-            rsync -avz --delete "$REMOTE:\"$SOURCE\"" "$DEST"
-        else
-            echo "Destination does not exist. Doing initial tar pull..."
-            mkdir -p "$DEST"
-            ssh "$REMOTE" "tar czf - -C \"$SOURCE\" ." |
-                tar xzf - -C "$DEST"
-        fi
-
-    else
-        echo "Invalid mode: use push or pull"
-        return 1
-    fi
 }
 
 # ---------------------
